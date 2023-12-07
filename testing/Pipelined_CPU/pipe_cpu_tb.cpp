f904044821b5ab89d1ae063341a71c8efd2cb9b1
Idrees <33992400+idrees-mahmood@users.noreply.github.com>
2023-12-07 03:19:56 +0000

    Pipelined CPU was not included

---
+++ testing/Pipelined_CPU/pipe_cpu_tb.cpp
@@ -0,0 +1,65 @@
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "Vpipelined_cpu.h" 
+#include <iostream>
+
+#include "vbuddy.cpp"     
+#define MAX_SIM_CYC 1000000
+
+int main(int argc, char **argv, char **env) {
+    int simcyc;
+    int tick;
+
+    Verilated::commandArgs(argc, argv);
+    // init top verilog instance
+    Vmaster* top = new Vmaster;
+    // init trace dump
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace (tfp, 99);
+    tfp->open ("CPU.vcd");
+
+    //init Vbuddy
+    if (vbdOpen()!=1) return(-1);
+    vbdHeader("CPU CW");
+
+    // intialise
+    top->clk = 1;
+    top->rst = 0;
+    top->trigger = 0;
+    top->testRegAddress = 21;
+
+    // run simulation for MAX_SIM_CYC clock cycles
+    for (simcyc=0; simcyc<MAX_SIM_CYC; simcyc++) {
+        // dump variables into VCD file and toggle clock
+        for (tick=0; tick<2; tick++) {
+            tfp->dump (2*simcyc+tick);
+            top->clk = !top->clk;
+            top->eval ();
+        }
+
+        // std::cout << "Mem word " << std::hex << top->Result << std::endl;
+
+        // send a0 value to 7 seg display
+        //vbdHex(4, ((top->Result) >> 16) & 0xF);
+        //vbdHex(3, ((top->Result) >> 8) & 0xF);
+        //vbdHex(2, ((top->Result) >> 4) & 0xF);
+        //vbdHex(1, top->Result & 0xF);
+
+        vbdBar(top->Result & 0xFF);
+
+        //if (top -> Result > 1){vbdPlot(top->Result, 0, 255);}
+
+
+        vbdCycle(simcyc);
+    
+        // either simulation finished, or 'q' is pressed
+        if (Verilated::gotFinish() || vbdGetkey()=='q')
+            exit(0);
+    }
+
+    vbdClose();
+    tfp->close();   
+    exit(0);
+
+}
