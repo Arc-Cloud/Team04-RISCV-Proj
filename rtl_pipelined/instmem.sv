ab340fea7eb814296db66034060a05ddfa124a7c
IlanIwumbwe <ikibfm@gmail.com>
2023-12-08 20:13:17 +0000

    All tests working except pdf :(

--- rtl_pipelined/instmem.sv
+++ rtl_pipelined/instmem.sv
@@ -13,14 +13,14 @@ logic [STORAGE_WIDTH-1:0] rom_array [32'hbfc00fff:32'hbfc00000];
 
 initial begin    
     $display("Loading instruction memory...");
-    $readmemh("/home/ilan/Desktop/Year2/Team04-RISCV-Proj/testing/Type B-J test/b_test.mem", rom_array);
+    $readmemh("/home/ilan/Desktop/Year2/Team04-RISCV-Proj/testing/Ref program test/pdf.mem", rom_array);
     $display("Instruction memory loaded....");// instr.mem to be preloaded depending on the program to be executed
 end
 
 always_comb begin
     addr = A[11:0];
     RD = {rom_array[addr], rom_array[addr+1], rom_array[addr+2], rom_array[addr+3]};
-    $display("current instr: %h, addr: %h", RD, addr);
+    // $display("current instr: %h, addr: %h", RD, addr);
 end
     
 endmodule
